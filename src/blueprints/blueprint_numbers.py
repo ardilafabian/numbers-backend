ec50d12faa1177687b3e93da0298b8a4981ae0ac
Fabián Ardila <fabian_ar97@hotmail.com>
2021-07-20 15:39:13 -0500

    :sparkles: Change methos api

--- src/blueprints/blueprint_numbers.py
+++ src/blueprints/blueprint_numbers.py
@@ -3,7 +3,7 @@ from src.controllers.numbers_controller import process_number_sum
 
 numbers = Blueprint(name="numbers", import_name=__name__)
 
-@numbers.route('/sum', methods=['GET'])
+@numbers.route('/sum', methods=['POST'])
 def process_sum():
     data = request.get_json()
     array = data['array']
